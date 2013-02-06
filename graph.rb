a72a8486cad3530f71c1993b8d723a89c47ad09e
James Dabbs <jdabbs@emcien.com>
2013-02-06 09:05:14 -0500

    Add author network and allow loading from file

--- graph.rb
+++ graph.rb
@@ -8,21 +8,24 @@ get '/' do
 end
 
 get '/data.json' do
-  begin
-    Steffi::Graph.instance_eval(params[:graph]).d3.to_json
-  rescue => e
-    { error:   true,
-      type:    e.class,
-      message: e.message }.to_json
+  Dir.chdir('data') do
+    begin
+      cmd = params[:graph].empty? ? 'famous :meredith' : params[:graph]
+      Steffi::Graph.instance_eval(cmd).d3.to_json
+    rescue => e
+      { error:   true,
+        type:    e.class,
+        message: e.message }.to_json
+    end
   end
 end
 
 get '/app.js' do
-  content_type "text/javascript"
+  content_type 'text/javascript'
   coffee :app
 end
 
 get '/public/:name.js' do |name|
-  content_type "text/javascript"
+  content_type 'text/javascript'
   File.read "public/#{name}.js"
 end
